49064eff9a92ac5bb46ce9c39fb8db8877a90d7d
張文相 Zhang Wenxiang (旋風之音 GoneTone) <p29022716@gmail.com>
2021-05-22 21:16:06 +0800

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GoneTone/1922-sms-real-link.git main:gh-pages
+git push -f git@github.com:GoneTone/1922-sms-real-link.git master:gh-pages
 
 cd -
